d56586b2b81e5babd13d03d93ae4d32661c5c63f
Prafull <prafull@arth.team>
2024-11-17 01:05:03 -0600

    wip in progress

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      script: 'dist/index.js',
+      instances: 2,
+      autorestart: true,
+      watch: true,
+      env: {
+        LOG_LEVEL: 'info',
+        DB_HOST: 'sustain-app.cji8semeum2o.us-east-2.rds.amazonaws.com',
+        DB_PORT: 5432,
+        DB_USER: 'postgres',
+        DB_PASSWORD: 'temp#123456!12',
+        DB_DATABASE: 'sustain',
+        DB_SCHEMA: 'public'
+
+      }
+    }
+  ]
+};
